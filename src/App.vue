f0b27b449af3b9e71e0f4a32b3dfe3d5222e5b2b
cym <yatmingtsai@foxmail.com>
2018-06-08 14:13:40 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,62 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import * as THREE from 'three'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      scene: null,
+      camera: null,
+      renderer: null,
+      geometry: null,
+      material: null,
+      cube: null
+    }
+  },
+  created () {
+    console.log('THREE: ', THREE)
+  },
+  mounted () {
+    this.init()
+    this.renderBox()
+    this.animate()
+  },
+  methods: {
+    init () {
+      this.scene = new THREE.Scene()
+      this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+      this.renderer = new THREE.WebGLRenderer()
+      this.renderer.setSize(window.innerWidth, window.innerHeight)
+      document.body.appendChild(this.renderer.domElement)
+    },
+    renderBox () {
+      this.geometry = new THREE.BoxGeometry( 1, 1, 1 )
+      this.material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+      this.cube = new THREE.Mesh(this.geometry, this.material)
+      this.scene.add(this.cube)
+      this.camera.position.z = 5
+    },
+    animate() {
+      requestAnimationFrame(this.animate)
+      this.cube.rotation.x += 0.03
+      this.cube.rotation.y += 0.03
+      this.renderer.render(this.scene, this.camera)
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
 }
-</style>
+canvas {
+  width: 100%;
+  height: 100%;
+}
+</style>  
